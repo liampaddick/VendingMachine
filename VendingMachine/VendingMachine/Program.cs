a678eb743254e87fdbfae4ef029810c777962aeb
liampaddick <liampaddick@users.noreply.github.com>
2019-03-02 15:11:30 +0000

    removed subtraction test code
    removed subtraction test code

--- VendingMachine/VendingMachine/Program.cs
+++ VendingMachine/VendingMachine/Program.cs
@@ -36,7 +36,6 @@ namespace VendingMachine
                     case 1: // add credit
                         Console.WriteLine("How much credit would you like to add?");
                         credit = addCredit(credit, float.Parse(Console.ReadLine()));
-                        credit = subtractCredit(credit, 25);
                         break;
                     case 2:
                         break;
