aaf806f5044b33db68d0158cc9e2d7e4bdf3c091
liampaddick <liampaddick@users.noreply.github.com>
2019-03-02 13:22:05 +0000

    Added base class for items as well as derived classes for food and drink
    Added base class for items as well as derived classes for food and drink.

--- VendingMachine/VendingMachine/Program.cs
+++ VendingMachine/VendingMachine/Program.cs
@@ -10,6 +10,116 @@ namespace VendingMachine
     {
         static void Main(string[] args)
         {
+            bool vendorOn = true;
+            float credit = 0.0f;
+
+            Console.WriteLine("This will simulate a vending machine");
+
+            Dog tempDog = new Dog();
+
+            tempDog.GetAge();
+
+            while (vendorOn)
+            {
+                Console.WriteLine("Current Credit: " + credit);
+
+                Console.WriteLine("What would you like to do? ");
+                Console.WriteLine("1 - Add Credit");
+                Console.WriteLine("2 - Get info on an item");
+                Console.WriteLine("3 - Dispense an Item");
+                Console.WriteLine("4 - Add new / Restock item");
+                Console.WriteLine("5 - Remove item / All item");
+                Console.WriteLine("6 - Shut down machine");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Item // base class that food and drink will inherit from
+    {
+        public int itemID;
+        public string category;
+        public float price;
+        public string nutritionalInfo;
+
+        public int GetItemID()
+        {
+            return itemID;
+        }
+        public string GetCategory()
+        {
+            return category;
+        }
+        public float GetPrice()
+        {
+            return price;
+        }
+        public string GetNutritionalInfo()
+        {
+            return nutritionalInfo;
+        }
+    }
+
+    class Snack : Item
+    {
+        float weight;
+
+        float GetWeight()
+        {
+            return weight;
+        }
+        
+    }
+
+    class Drink : Item
+    {
+        float capacity;
+
+        float GetCapacity()
+        {
+            return capacity;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    class Animal
+    {
+        string name;
+        int age;
+        float happiness;
+
+        public void PrintBaseValues()
+        {
+            Console.WriteLine(name + " "  + age + " " + happiness);
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+    }
+
+    class Dog : Animal
+    {
+        public int spotCount;
+        public void Bark ()
+        {
+            Console.WriteLine("Woof");
+        }
+
+        public int DogGetAge()
+        {
+            return (GetAge());
         }
     }
 }
